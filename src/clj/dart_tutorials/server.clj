e16f9ad4f368922c909179169f3c88c0be8280d4
Daniel Solano Gómez <daniel@solanogomez.org>
2014-02-02 08:12:27 -0600

    Add web app index

--- src/clj/dart_tutorials/server.clj
+++ src/clj/dart_tutorials/server.clj
@@ -1,9 +1,11 @@
 (ns dart-tutorials.server
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :refer [redirect]]))
 
 (defroutes routes
+  (GET "/" [] (redirect "/index.html"))
   (route/resources "/")
   (route/not-found "Page not found"))
 
